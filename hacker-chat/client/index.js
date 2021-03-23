386e221ce6360dc67f360e724790b9a110ca5bc0
unknown <weeslleey354@gmail.com>
2021-03-23 14:56:32 -0300

    finish server

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -7,12 +7,15 @@
 
 import Events from "events";
 import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socker.js";
 import TerminalController from "./src/terminalController.js";
 
 const [nodePath, filePath, ...comands] = process.argv;
 const config = CliConfig.parseArguments(comands);
 console.log("config", config);
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
 
 //const controller = new TerminalController();
 //await controller.initializeTable(componentEmitter);
